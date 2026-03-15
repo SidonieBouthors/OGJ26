71a9ae63930f53499cc58956f92f1735245e6bcb
Clément Josso <clement.josso@epfl.ch>
2026-03-15 12:40:59 +0100

    background + minor modifs

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,14 @@
-extends PanelContainer
+extends Control
 class_name MainMenu
+
+
+func _ready():
+	$Background.autoplay
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
+
 
 func on_play():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
